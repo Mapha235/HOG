0ee4ea847754a2248208d8ba02da4a7ede05c2c5
MartinP <phammartin235@gmail.com>
2021-11-20 02:50:04 +0100

    Added block normalization, visualise function(copied). Imported hog from scikit image lib for comparison. ISSUE:  Histograms are tilted by 90 deg.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
+from numpy.lib.function_base import interp
+from skimage.io import imread
+from skimage import draw
+from skimage import exposure
+from skimage.feature import hog
+from skimage.transform import resize
+from skimage.feature import _hoghistogram
+
+from numpy.core.shape_base import block
 import scipy.ndimage as nd
 import scipy.signal
 import matplotlib.pyplot as plt
@@ -56,9 +65,6 @@ def preprocess(img: np.ndarray, normalize=False):
     return cv2.resize(img, size, interpolation=cv2.INTER_AREA)
 
 
-def l2_norm():
-    pass
-
 # GRADIENT COMPUTATION------------------------------------------------------------------------------------------------
 
 
@@ -101,7 +107,7 @@ def compute_gradients(img):
             res_ang[i, j] = (res_ang[i, j] * 360) / (2*np.pi)
             # res_ang[i, j] =
     # return np.hypot(grad_x[0], grad_y[0]), np.hypot(grad_x[1], grad_y[1]), np.hypot(grad_x[2], grad_y[2]), res_magn
-    return res_magn, res_ang
+    return grad_x[0], grad_y[0], res_magn, res_ang
 
 # ORIENTATION BINNING------------------------------------------------------------------------------------------------
 
@@ -135,11 +141,13 @@ def cells(magnitudes, orientations, nr_of_bins=9, cell_size=8):
 
 def interpolate(magnitude: float, orientation: float, nr_of_bins=9, signed=True):
     '''
-    
+    Splits the magnitude value into left and right value according to their contribution to the adjacent bins.
     '''
     steps = 360 / nr_of_bins
     if signed:
         steps = steps / 2
+
+    orientation = int(orientation)
 
     l_bin = int(orientation/steps) % 9
     r_bin = (int(orientation/steps) + 1) % 9
@@ -156,11 +164,11 @@ def interpolate(magnitude: float, orientation: float, nr_of_bins=9, signed=True)
 def binning(cells_magn, cells_ang):
     if len(cells_magn) != len(cells_ang):
         return 1
-    
+
     histogram = np.zeros(shape=(len(cells_magn), 9))
+
     # histogram = np.zeros(9)
 
-    
     # iterate over the cells
     for cell, ang, hist_index in zip(cells_magn, cells_ang, range(len(cells_magn))):
         if len(cell) != len(ang):
@@ -169,24 +177,112 @@ def binning(cells_magn, cells_ang):
         # loop through the cell values
         for i in range(len(cell)):
             for j in range(len(cell[i])):
-                left, right = interpolate(cell[i][j], ang[i][j], nr_of_bins=9, signed=True)                
+                left, right = interpolate(
+                    cell[i][j], ang[i][j], nr_of_bins=9, signed=True)
+
                 histogram[hist_index][left[0]] += left[1]
                 histogram[hist_index][right[0]] += right[1]
-                
-    return histogram
 
-def block_normalization(blocks):
-    pass
+    return histogram.reshape(16, 8, 9)
 
+
+def l2_norm(vector):
+    '''
+    Returns the L2-normalized vector.
+    '''
+    squared = [val ** 2 for val in vector]
+    norm = np.sqrt(sum(squared))
+    return [val / norm for val in vector]
+
+
+def block_normalization(block_hists, nr_of_bins=9, stride=8):
+    '''
+    Returns the normalized
+    '''
+    rows, columns, bins = block_hists.shape
+    # feature_descriptor = np.zeros(shape= ())
+    feature_descriptor = []
+
+    for row in range(rows-1):
+        for column in range(columns-1):
+            # block.append(np.concatenate([block_hists[row][column]  , block_hists[row][column+1],
+            #                              block_hists[row+1][column], block_hists[row+1][column+1]]))
+            block_norm = l2_norm(np.concatenate([block_hists[row][column], block_hists[row][column+1],
+                                                 block_hists[row+1][column], block_hists[row+1][column+1]]))
+            feature_descriptor = feature_descriptor + block_norm
+
+    return feature_descriptor
+
+# SKIIMAGE ----------------------------------------------------------------------------------------------------------
+
+
+# VISUALIZE ---------------------------------------------------------------------------------------------------------
+# copied from the scitkit-image library: https://github.com/scikit-image/scikit-image/blob/main/skimage/feature/_hog.py
+
+def visualize(g_row, g_col, orientation_histogram):
+
+    hog_image = None
+    # s_row, s_col = img.shape[:2]
+    s_row, s_col = (128, 64)
+    c_row, c_col = (8, 8)
+    b_row, b_col = (2, 2)
+    orientations = 9
+
+    n_cells_row = int(s_row // c_row)  # number of cells along row-axis
+    n_cells_col = int(s_col // c_col)  # number of cells along col-axis
+    
+    plt.figure()
+    plt.bar(np.arange(0,9), orientation_histogram[0][0])
+
+    # compute orientations integral images
+    orientation_histogram = np.zeros((n_cells_row, n_cells_col, orientations),
+                                    dtype=float)
+    g_row = g_row.astype(float, copy=False)
+    g_col = g_col.astype(float, copy=False)
+
+    _hoghistogram.hog_histograms(g_col, g_row, c_col, c_row, s_col, s_row,
+                                n_cells_col, n_cells_row,
+                                orientations, orientation_histogram)
+
+    plt.figure()
+    plt.bar(np.arange(0,9), orientation_histogram[0][0])
+    plt.show()
+
+    radius = min(c_row, c_col) // 2 - 1
+    orientations_arr = np.arange(orientations)
+    # set dr_arr, dc_arr to correspond to midpoints of orientation bins
+    orientation_bin_midpoints = (
+        np.pi * (orientations_arr + .5) / orientations)
+
+    dr_arr = radius * np.sin(orientation_bin_midpoints)
+    dc_arr = radius * np.cos(orientation_bin_midpoints)
+
+    hog_image = np.zeros((s_row, s_col), dtype=float)
+
+    for r in range(n_cells_row):
+        for c in range(n_cells_col):
+            for o, dr, dc in zip(orientations_arr, dr_arr, dc_arr):
+                centre = tuple([r * c_row + c_row // 2,
+                                c * c_col + c_col // 2])
+                rr, cc = draw.line(int(centre[0] - dc),
+                                   int(centre[1] + dr),
+                                   int(centre[0] + dc),
+                                   int(centre[1] - dr))
+                hog_image[rr, cc] += orientation_histogram[r][c][o]
+
+    return hog_image
 
 # MAIN---------------------------------------------------------------------------------------------------------------
+
+
 def main():
-    img = cv2.imread('./data/man.png')
+    img = cv2.imread('./data/b.png')
+    # img = cv2.imread('./data/man.png')
     # img = cv2.imread('./data/pedestrians.jpg')
     # plot(img, 'normal')
 
     # Create a black image
-    # img = np.zeros((640, 480, 3))add
+    # img = np.zeros((640, 480, 3))
     # # ... and make a white rectangle in it
     # img[100: -100, 80: -80] = 1
 
@@ -194,16 +290,33 @@ def main():
     # plot(img, 'resized')
 
     # grad_red, grad_green, grad_blue, grad_mag = compute_gradients(img)
-    grad_mag, grad_ang = compute_gradients(img)
+    g_row, g_col, grad_mag, grad_ang = compute_gradients(img)
 
     plot(grad_mag, 'grad opt')
     # plot(grad_ang, 'grad ang')
     cells_mag, cells_ang = cells(grad_mag, grad_ang)
     hist = binning(cells_mag, cells_ang)
-    # plt.figure()
-    # plt.hist(hist[0], bins = np.arange(0,9))
 
-    # plot(convolve_sobel(img), 'convolve')
+    plot(visualize(g_row, g_col, hist), 'HG')
+    final_descr = block_normalization(hist)
+
+    # free up memory
+    grad_mag = []
+    grad_ang = []
+    cells_mag = []
+    cells_ang = []
+    hist = []
+
+    fd, hog_image = hog(img, orientations=9, pixels_per_cell=(8, 8),
+                        cells_per_block=(2, 2), visualize=True, multichannel=True)
+    plt.axis("off")
+    plot(hog_image, 'HOG')
+    
+    # plt.figure()
+    # plt.hist(final_descr)
+    # plt.figure()
+    # plt.hist(fd)
+
     plt.show()
 
 
